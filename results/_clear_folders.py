47174cf45e98d9d1af1c1da072d7cd485c0fa500
matthew-lemon-tamu <matthewlemon@tamu.edu>
2024-10-11 17:53:40 -0500

    Small Changes to improve usability

--- results/_clear_folders.py
+++ results/_clear_folders.py
@@ -1,17 +1,21 @@
 import os
 
-files = os.listdir("./results/pred_E/")
-for f in files:
-    os.remove("./results/pred_E/" + f)
-    
-files = os.listdir("./results/pred_v/")
-for f in files:
-    os.remove("./results/pred_v/" + f)
+def clear_folders():
+    files = os.listdir("./results/pred_E/")
+    for f in files:
+        os.remove("./results/pred_E/" + f)
+        
+    files = os.listdir("./results/pred_v/")
+    for f in files:
+        os.remove("./results/pred_v/" + f)
 
-files = os.listdir("./results/pred_ux/")
-for f in files:
-    os.remove("./results/pred_ux/" + f)
+    files = os.listdir("./results/pred_ux/")
+    for f in files:
+        os.remove("./results/pred_ux/" + f)
+        
+    files = os.listdir("./results/pred_uy/")
+    for f in files:
+        os.remove("./results/pred_uy/" + f)
     
-files = os.listdir("./results/pred_uy/")
-for f in files:
-    os.remove("./results/pred_uy/" + f)
+if __name__ == "__main__":
+    clear_folders()
